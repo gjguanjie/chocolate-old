109ffcec4f087f5a1a02c89ca5ed3d0f640b58a0
gjguanjie@sina.com <gjguanjie@sina.com>
2019-09-02 21:30:06 +0800

    修改login 校验，注意，校验为异常校验

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,36 +1,42 @@
 <template>
   <div class="full-container">
-      <el-form :model="loginForm" :rules="loginRules" ref="loginForm" label-position="left" label-width="0px"
-               class="login-container">
-        <h2 class="title" style="padding-left:22px;">登录系统</h2>
-        <el-form-item prop="username">
-          <span><svg-icon icon-class="user"/></span>
-          <el-input type="text" v-model="loginForm.username" auto-complete="off" placeholder="请输入账号"></el-input>
-        </el-form-item>
-        <el-form-item prop="password">
-          <svg-icon icon-class="password"/>
-          <el-input type="password" v-model="loginForm.password" auto-complete="off" placeholder="请输入密码"></el-input>
-        </el-form-item>
-        <el-form-item>
-          <el-col :span="12">
-            <el-form-item prop="captcha">
-              <el-input type="test" v-model="loginForm.captcha" auto-complete="off" placeholder="验证码, 单击图片刷新"
-                        style="width: 100%;">
-              </el-input>
-            </el-form-item>
-          </el-col>
-          <el-col class="line" :span="1">&nbsp;</el-col>
-          <el-col :span="11">
-            <el-form-item>
-              <img style="width: 100%;" class="pointer" :src="loginForm.src" @click="refreshCaptcha">
-            </el-form-item>
-          </el-col>
-        </el-form-item>
-        <el-form-item style="width:100%;">
-          <el-button type="success" style="width:48%;" @click.native.prevent="reset">重 置</el-button>
-          <el-button type="success" style="width:48%;" @click.native.prevent="login('loginForm')" :loading="loading">登 录</el-button>
+    <el-form :model="loginForm" :rules="loginRules" ref="loginForm" label-position="left" label-width="0px"
+             class="login-container">
+      <h2 class="title" style="padding-left:22px;">登录系统</h2>
+      <el-form-item prop="username">
+        <!--<span><svg-icon icon-class="user"/></span>-->
+        <el-input type="text" v-model="loginForm.username" auto-complete="off" placeholder="请输入账号"></el-input>
+      </el-form-item>
+      <el-form-item prop="password">
+        <!--<svg-icon icon-class="password"/>-->
+        <el-input type="password" v-model="loginForm.password" auto-complete="off" placeholder="请输入密码"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-col :span="12">
+          <el-form-item prop="captcha">
+            <el-input type="test" v-model="loginForm.captcha" auto-complete="off" placeholder="验证码, 单击图片刷新"
+                      style="width: 100%;">
+            </el-input>
+          </el-form-item>
+        </el-col>
+        <el-col class="line" :span="1">&nbsp;</el-col>
+        <el-col :span="11">
+          <el-form-item>
+            <img style="width: 100%;" class="pointer" :src="loginForm.src" @click="refreshCaptcha">
+          </el-form-item>
+        </el-col>
+      </el-form-item>
+      <el-form-item style="width:100%;">
+         <el-button type="info" style="width:48%;" @click.native.prevent="reset">重 置</el-button>
+        <el-button type='info' style="width:48%;" @click.native.prevent="login('loginForm')" :loading="loading">登 录</el-button>
         </el-form-item>
       </el-form>
+      <el-dialog :visible.sync="dialogVisible" width="30%" modal lock-scroll center>
+        <span>用户名或密码错误，请重新输入......</span>
+        <span slot="footer" class="dialog-footer">
+            <el-button type="primary" @click="dialogVisible = false">关闭</el-button>
+        </span>
+      </el-dialog>
   </div>
 </template>
 
@@ -39,11 +45,11 @@ export default {
   name: 'Login',
   data () {
     return {
+      dialogVisible: false,
       loading: false,
       loginForm: {
         username: '',
         password: '',
-        captcha: '',
         src: ''
       },
       loginRules: {
@@ -53,27 +59,30 @@ export default {
         password: [
           { required: true, message: '请输入密码', trigger: 'blur' }
         ]
-      },
-      checked: true
+      }
     }
   },
   methods: {
     login (loginForm) {
-      console.log('login')
       this.$refs[loginForm].validate((valid) => {
         if (!valid) {
-          console.log('error submit!!')
           return false
+        } else {
+          debugger
+          if (this.loginForm.username === 'admin' && this.loginForm.password === 'admin') {
+            sessionStorage.setItem('username', this.username)
+            this.$router.push('/home')
+          } else {
+            this.dialogVisible = true
+          }
         }
       })
-      sessionStorage.setItem('username', this.username)
-      this.$router.push('/home')
-    },
-    refreshCaptcha: function () {
-      this.loginForm.src = new Date().getTime()
     },
     reset () {
       this.$refs.loginForm.resetFields()
+    },
+    refreshCaptcha: function () {
+      this.loginForm.src = new Date().getTime()
     }
   },
   mounted () {
@@ -84,7 +93,7 @@ export default {
 
 <style lang="scss" scoped>
   .full-container {
-    background-color: beige;
+    background-color: #394056;
     background-size: 100% 100%;
     position: absolute;
     top: 0px;
