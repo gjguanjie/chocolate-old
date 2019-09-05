c18702fdeb366459cc2ce9cf9bbe126170ad8898
gjguanjie@sina.com <gjguanjie@sina.com>
2019-09-05 20:52:23 +0800

    mock仅development时生效

--- src/main.js
+++ src/main.js
@@ -8,11 +8,10 @@ import './icons'
 import ElementUI from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/en'
 import './styles.scss'
-import './mock'
-
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 Vue.use(VueAxios, axios)
+process.env.NODE_ENV === 'development' && require('./mock')
 
 Vue.use(ElementUI, { locale })
 Vue.config.productionTip = false
